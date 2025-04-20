3a602a58aa85d2e47dbc204aef69360d47ccf8ba
Grace-tech1989 <graceomowummie@gmail.com>
2025-04-20 23:47:28 +0100

    Added search function using API

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
 
+  temperatureElement.innerHTML = Math.round(temperature);
+  cityElement.innerHTML = response.data.city;
+}
 
-
-
-
+function searchCity(city) {
+  let apiKey = "871f3b39d0dbo359344c4192ce44tca2";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
 
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   console.log(searchInput.value);
-  let cityElement =  document.querySelector("#city")
-   cityElement.inn = searchInput.value
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("nigeria");
