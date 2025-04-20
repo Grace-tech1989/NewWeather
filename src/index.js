07bafd3823b9a6b4e7e247dc6da92aab2061b805
Grace-tech1989 <graceomowummie@gmail.com>
2025-04-20 17:42:07 +0100

    Added Axious

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
+
+
+
+
+
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   console.log(searchInput.value);
   let cityElement =  document.querySelector("#city")
-   cityElement.innerHTML = searchInput.value
+   cityElement.inn = searchInput.value
 }
 
 let searchFormElement = document.querySelector("#search-form");
